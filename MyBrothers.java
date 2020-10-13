3aa11f20a5ff39fbb91ee1c870d84e865d20637f
driellyg <31835969+driellyg@users.noreply.github.com>
2020-10-13 17:16:57 -0300

    Delete MyBrothers.java

--- MyBrothers.java
+++
@@ -1,7 +0,0 @@
-public class MyBrothers {
-  int x = 1;
-  public static void main(String[] args) {
-    MyBrothers myObj = new MyBrothers();
-    System.out.println("Minha quantidade de irmao(s) e: " + myObj.x);
-  }
-}
