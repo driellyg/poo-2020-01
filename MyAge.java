f8fd5c0445563e0173932129ece64a4edaf3acf3
driellyg <31835969+driellyg@users.noreply.github.com>
2020-10-13 17:14:00 -0300

    Add files via upload

---
+++ MyAge.java
@@ -0,0 +1,9 @@
+public class MyAge {
+  int x = 21;
+
+  public static void main(String[] args) {
+    MyAge myObj = new MyAge();
+    System.out.println("Minha idade e");
+    System.out.println(myObj.x);
+  }
+}
