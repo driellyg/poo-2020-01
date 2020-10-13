931eb953221c2bcaad1de50e32fed5f836c81018
driellyg <31835969+driellyg@users.noreply.github.com>
2020-10-13 17:16:36 -0300

    Delete MyAge.java

--- MyAge.java
+++
@@ -1,9 +0,0 @@
-public class MyAge {
-  int x = 21;
-
-  public static void main(String[] args) {
-    MyAge myObj = new MyAge();
-    System.out.println("Minha idade e");
-    System.out.println(myObj.x);
-  }
-}
